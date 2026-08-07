173e126a5cae2c3a1b44a4abf6524c344b3f58bb
SickProdigy <prodigysick@gmail.com>
2026-08-07 17:24:37 +0000

    Add Social Meta Tags plugin for Open Graph and Twitter metadata

---
+++ Upload/inc/plugins/social_meta_tags.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * Social Meta Tags
+ *
+ * Open Graph and Twitter metadata for MyBB pages.
+ */
+
+if (!defined('IN_MYBB')) {
+    die('Direct initialization of this file is not allowed.');
+}
+
+function social_meta_tags_info()
+{
+    return array(
+        'name' => 'Social Meta Tags',
+        'description' => 'Provides configurable Open Graph and Twitter metadata variables for MyBB templates.',
+        'website' => 'https://www.sickgaming.net',
+        'author' => 'Sick Gaming',
+        'authorsite' => 'https://www.sickgaming.net',
+        'version' => '1.0.0',
+        'compatibility' => '18*'
+    );
+}
+
+function social_meta_tags_install()
+{
+    social_meta_tags_ensure_settings();
+}
+
+function social_meta_tags_is_installed()
+{
+    global $db;
+
+    $query = $db->simple_select('settinggroups', 'gid', "name='social_meta_tags'");
+    $group = $db->fetch_array($query);
+
+    return !empty($group['gid']);
+}
+
+function social_meta_tags_uninstall()
+{
+    global $db;
+
+    $query = $db->simple_select('settinggroups', 'gid', "name='social_meta_tags'");
+    $group = $db->fetch_array($query);
+
+    if (!empty($group['gid'])) {
+        $gid = (int)$group['gid'];
+        $db->delete_query('settings', "gid='{$gid}'");
+        $db->delete_query('settinggroups', "gid='{$gid}'");
+        social_meta_tags_rebuild_settings();
+    }
+}
+
+function social_meta_tags_activate()
+{
+    social_meta_tags_ensure_settings();
+}
+
+function social_meta_tags_deactivate()
+{
+}
+
+function social_meta_tags_ensure_settings()
+{
+    global $db;
+
+    $query = $db->simple_select('settinggroups', 'gid', "name='social_meta_tags'");
+    $group = $db->fetch_array($query);
+
+    if (!empty($group['gid'])) {
+        $gid = (int)$group['gid'];
+    } else {
+        $gid = (int)$db->insert_query('settinggroups', array(
+            'name' => 'social_meta_tags',
+            'title' => 'Social Meta Tags',
+            'description' => 'Settings for Open Graph and Twitter metadata.',
+            'disporder' => 1,
+            'isdefault' => 0
+        ));
+    }
+
+    $legacy_description = social_meta_tags_legacy_setting('revolution_theme_default_meta');
+    $legacy_image_url = social_meta_tags_legacy_setting('revolution_theme_logo_url');
+
+    $settings = array(
+        array(
+            'name' => 'social_meta_tags_default_description',
+            'title' => 'Default Meta Description',
+            'description' => 'Fallback description when a forum description is unavailable.',
+            'optionscode' => 'textarea',
+            'value' => $legacy_description !== ''
+                ? $legacy_description
+                : 'Sick Gaming is an exclusive community for console and PC enthusiasts. Connect with like-minded people to compete, program, develop, mod, and more.',
+            'disporder' => 1,
+            'gid' => $gid
+        ),
+        array(
+            'name' => 'social_meta_tags_default_image_url',
+            'title' => 'Default Image URL',
+            'description' => 'Absolute fallback image URL used for Open Graph and Twitter cards.',
+            'optionscode' => 'text',
+            'value' => $legacy_image_url !== ''
+                ? $legacy_image_url
+                : 'https://www.sickgaming.net/images/logo.png',
+            'disporder' => 2,
+            'gid' => $gid
+        )
+    );
+
+    foreach ($settings as $setting) {
+        $query = $db->simple_select('settings', 'sid', "name='" . $db->escape_string($setting['name']) . "'");
+        $existing = $db->fetch_array($query);
+
+        if (empty($existing['sid'])) {
+            $db->insert_query('settings', $setting);
+        }
+    }
+
+    social_meta_tags_rebuild_settings();
+}
+
+function social_meta_tags_legacy_setting($name)
+{
+    global $db;
+
+    $query = $db->simple_select('settings', 'value', "name='" . $db->escape_string($name) . "'");
+    $setting = $db->fetch_array($query);
+
+    return isset($setting['value']) ? trim($setting['value']) : '';
+}
+
+$plugins->add_hook('global_start', 'social_meta_tags_build');
+$plugins->add_hook('forumdisplay_start', 'social_meta_tags_build');
+$plugins->add_hook('showthread_start', 'social_meta_tags_build');
+function social_meta_tags_build()
+{
+    global $thread, $forum, $mybb;
+    global $open_meta_title, $open_meta_description, $open_meta_url, $open_meta_type, $open_meta_image;
+
+    $board_name = isset($mybb->settings['bbname']) ? $mybb->settings['bbname'] : '';
+    $board_url = isset($mybb->settings['bburl']) ? rtrim($mybb->settings['bburl'], '/') : '';
+
+    $open_meta_title = $board_name;
+    $open_meta_description = !empty($mybb->settings['social_meta_tags_default_description'])
+        ? $mybb->settings['social_meta_tags_default_description']
+        : '';
+    $open_meta_url = $board_url;
+    $open_meta_type = 'website';
+    $open_meta_image = !empty($mybb->settings['social_meta_tags_default_image_url'])
+        ? $mybb->settings['social_meta_tags_default_image_url']
+        : '';
+
+    if (!empty($forum['fid'])) {
+        $open_meta_title = $forum['name'] . ($board_name !== '' ? ' - ' . $board_name : '');
+        $open_meta_description = !empty($forum['description'])
+            ? $forum['description']
+            : $open_meta_description;
+        $open_meta_url = social_meta_tags_absolute_url(get_forum_link((int)$forum['fid']), $board_url);
+    }
+
+    if (!empty($thread['tid'])) {
+        $open_meta_title = $thread['subject'] . ($board_name !== '' ? ' - ' . $board_name : '');
+        $open_meta_url = social_meta_tags_absolute_url(get_thread_link((int)$thread['tid']), $board_url);
+        $open_meta_type = 'article';
+
+        if (!empty($thread['image'])) {
+            $open_meta_image = $thread['image'];
+        }
+    }
+
+    $open_meta_title = htmlspecialchars_uni(strip_tags($open_meta_title));
+    $open_meta_description = htmlspecialchars_uni(strip_tags($open_meta_description));
+    $open_meta_url = htmlspecialchars_uni($open_meta_url);
+    $open_meta_type = htmlspecialchars_uni($open_meta_type);
+    $open_meta_image = htmlspecialchars_uni($open_meta_image);
+}
+
+function social_meta_tags_absolute_url($url, $board_url)
+{
+    if (preg_match('#^https?://#i', $url)) {
+        return $url;
+    }
+
+    return $board_url . '/' . ltrim($url, '/');
+}
+
+function social_meta_tags_rebuild_settings()
+{
+    if (!function_exists('rebuild_settings')) {
+        require_once MYBB_ROOT . 'inc/functions.php';
+    }
+
+    rebuild_settings();
+}
